e1603ea35a18dcdf5058b57b5d72c8e40bd447e4
Leon <leon.karho.chung@gmail.com>
2017-01-18 14:36:03 -0500

    create class, def init

---
+++ cats-meow/exercise2.rb
@@ -0,0 +1,9 @@
+# define class Cat
+class Cat
+# instant variables
+  def initialize(name, preferred_food, meal_time)
+    @name = name
+    @preferred_food = preferred_food
+    @meal_time = meal_time
+  end
+end
